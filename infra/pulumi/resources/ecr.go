28a2676125a1df257b82f17437208dbdc13fad63
carlosEA28 <carloseaduarte28@gmail.com>
2026-03-29 19:25:27 -0300

    fix: Remove Docker image build from Pulumi, use plain ECR repository only

--- infra/pulumi/resources/ecr.go
+++ infra/pulumi/resources/ecr.go
@@ -1,41 +1,34 @@
 package resources
 
 import (
-	"github.com/pulumi/pulumi-awsx/sdk/v3/go/awsx/ecr"
+	awsecr "github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ecr"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type ECROutput struct {
-	Repository *ecr.Repository
-	Image      *ecr.Image
+	Repository *awsecr.Repository
 	ImageURI   pulumi.StringOutput
 }
 
 func CreateECR(ctx *pulumi.Context) (*ECROutput, error) {
-	repository, err := ecr.NewRepository(ctx, "ecom-api-repo", &ecr.RepositoryArgs{
+	repository, err := awsecr.NewRepository(ctx, "ecom-api-repo", &awsecr.RepositoryArgs{
 		ForceDelete: pulumi.Bool(true),
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	image, err := ecr.NewImage(ctx, "ecom-api-image", &ecr.ImageArgs{
-		RepositoryUrl: repository.Url,
-		Context:       pulumi.String(".."),
-		Dockerfile:    pulumi.String("../Dockerfile"),
-		Platform:      pulumi.String("linux/amd64"),
-	})
-	if err != nil {
-		return nil, err
-	}
+	// Use the repository URL as the image URI (will be tagged with 'latest' or git sha in CI/CD)
+	imageUri := repository.RepositoryUrl.ApplyT(func(url interface{}) string {
+		return url.(string) + ":latest"
+	}).(pulumi.StringOutput)
 
 	return &ECROutput{
 		Repository: repository,
-		Image:      image,
-		ImageURI:   image.ImageUri,
+		ImageURI:   imageUri,
 	}, nil
 }
 
 func GetECRRepositoryUrl(ecrOutput *ECROutput) pulumi.StringOutput {
-	return ecrOutput.Repository.Url
+	return ecrOutput.Repository.RepositoryUrl
 }
